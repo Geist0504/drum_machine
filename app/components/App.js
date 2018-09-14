2a72c3ad167f18dd9b8824c6e0effbf0bd75a580
Andrew Gallin <andrewgallin@gmail.com>
2018-09-14 08:14:10 -0400

    created first component and got compiling

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import css from './css/main.css'
+
+
+class Application extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+	}
+  render(){
+  	return(<div id='drum-machine'></div>)
+  }
+}
+
+export default Application;
