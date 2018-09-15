1cc5e16779a8d509f302c9e5bcf32edc8760fa16
Andrew Gallin <andrewgallin@gmail.com>
2018-09-15 16:31:06 -0400

    added drum buttons

--- app/components/App.js
+++ app/components/App.js
@@ -2,13 +2,56 @@ import React from 'react'
 import scss from './css/main.scss'
 
 
+class DrumButton extends React.Component{
+	constructor(props) {
+    super(props)
+    this.state = {}
+	}
+  render(){
+  	return(
+  		<div className='drum-button'><p>{this.props.press}</p></div>
+  		)
+  }
+}
+
+class DrumButtons extends React.Component{
+	constructor(props) {
+    super(props)
+    this.state = {}
+    
+	}
+  render(){
+  	  	const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+    	const buttons = keys.map((key) => <DrumButton press = {key} />)
+    	console.log(buttons)
+  	return(
+  		<div id='drum-buttons'>{buttons }</div>
+  		)
+  }
+}
+
+class MachineButtons extends React.Component{
+	constructor(props) {
+    super(props)
+    this.state = {}
+	}
+  render(){
+  	return(
+  		<div id='machine-buttons'></div>
+  		)
+  }
+}
+
 class Application extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
 	}
   render(){
-  	return(<div id='drum-machine'></div>)
+  	return(<div id='drum-machine'>
+  		<DrumButtons />
+  		<MachineButtons />
+  		</div>)
   }
 }
 
