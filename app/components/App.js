8068af5c41aa3ff649f02b261edcce10fe544324
Andrew Gallin <andrewgallin@gmail.com>
2018-09-16 11:09:31 -0400

    added key event listener to drum button component

--- app/components/App.js
+++ app/components/App.js
@@ -5,7 +5,20 @@ import scss from './css/main.scss'
 class DrumButton extends React.Component{
 	constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+    }
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+	}
+	componentDidMount(){
+		document.addEventListener('keydown', this.handleKeyPress)
+	}
+	componentWillUnmount(){
+		document.removeEventListener('keydown', this.handleKeyPress)
+	}
+  handleKeyPress(target) {
+	    if(target.key==this.props.press | target.key == this.props.press.toLowerCase()){
+	            console.log(target.key);    
+	    }
 	}
   render(){
   	return(
