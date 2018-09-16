a1c619b51f43736defd27fc7e510bb382f38de9d
Andrew Gallin <andrewgallin@gmail.com>
2018-09-16 11:44:18 -0400

    added button press styling

--- app/components/App.js
+++ app/components/App.js
@@ -2,41 +2,44 @@ import React from 'react'
 import scss from './css/main.scss'
 
 
-class DrumButton extends React.Component{
-	constructor(props) {
+class DrumButton extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
+    	pressed: false
     }
     this.handleKeyPress = this.handleKeyPress.bind(this)
-	}
-	componentDidMount(){
-		document.addEventListener('keydown', this.handleKeyPress)
-	}
-	componentWillUnmount(){
-		document.removeEventListener('keydown', this.handleKeyPress)
-	}
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress)
+  }
   handleKeyPress(target) {
-	    if(target.key==this.props.press | target.key == this.props.press.toLowerCase()){
-	            console.log(target.key);    
+	    if (target.key === this.props.press | target.key === this.props.press.toLowerCase()) {
+	        console.log(target.key)
+	        this.setState({pressed: true})
+	        console.log(this.state)
 	    }
-	}
-  render(){
-  	return(
-  		<div className='drum-button'><p>{this.props.press}</p></div>
+  }
+  render() {
+  	return (
+  		<button className={this.state.pressed ? 'drum-button drum-button-pushed' : 'drum-button'}>
+  		<p>{this.props.press}</p></button>
   		)
   }
 }
 
-class DrumButtons extends React.Component{
-	constructor(props) {
+class DrumButtons extends React.Component {
+  constructor(props) {
     super(props)
-    this.state = {}
-    
-	}
-  render(){
+    this.state = {}  
+  }
+  render() {
   	  	const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
     	const buttons = keys.map((key) => <DrumButton press = {key} />)
-  	return(
+  	return (
   		<div id='drum-buttons'>{buttons }</div>
   		)
   }
